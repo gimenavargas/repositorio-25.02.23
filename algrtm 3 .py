f5e9107379ae2171605d8dbac02b1677a7e5f56c
gimenavargas <1471366@senati.pe>
2023-02-25 10:14:45 -0500

    algoritmo indicando numero

---
+++ algrtm 3 .py
@@ -0,0 +1,7 @@
+# desarrollar algoritmo indicando numeros
+
+n = int(input("Introduce la altura del triángulo (entero positivo): "))
+print("-------------------")
+for i in range(n):
+   print(str(i) *i)
+print("-------------------")
