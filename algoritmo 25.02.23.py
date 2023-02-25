56435262cdc6e5a8f04bf804100449b649a9412b
gimenavargas <1471366@senati.pe>
2023-02-25 10:05:57 -0500

    bucle de numeros

--- algoritmo 25.02.23.py
+++ algoritmo 25.02.23.py
@@ -1,8 +1,15 @@
-#DESARROLLAR UN ALGORITMO QUE MUESTRE EL SIGUIENTE PATRON
+#DESARROLLAR UN ALGORITMO QUE MUESTRE EL SIGUIENTE PATRON en python
+1
+22
+333
+4444
 
 
-x = 1 
+x = 1
 while x<6:
     print(x)
-    x+= 1,2,3
+    x+= 1
 print("tc")
+
+
+
